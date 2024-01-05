3b8a454f3f5a020de2acebd95df7e62f8be83e4d
vsmm-world <thekingjarvis792@gmail.com>
2024-01-05 16:35:41 +0530

    creating base model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomerModule } from './customer/customer.module';
+import { ProductsModule } from './products/products.module';
+import { CategoryModule } from './category/category.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, PrismaModule, AuthModule, CustomerModule, ProductsModule, CategoryModule, PaymentModule],
   controllers: [],
   providers: [],
 })
