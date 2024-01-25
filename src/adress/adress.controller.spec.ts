abe94f0cfd8233f10647b6f1f5d26ea556a4f01e
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2024-01-25 14:34:41 +0530

    updates in test cases

--- src/adress/adress.controller.spec.ts
+++ src/adress/adress.controller.spec.ts
@@ -7,7 +7,6 @@ import { NotFoundException } from '@nestjs/common';
 
 describe('AdressController', () => {
   let controller: AdressController;
-  let service: AdressService;
 
   const notFound = {
     statusCode: 404,
@@ -58,7 +57,6 @@ describe('AdressController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-
       controllers: [AdressController],
       providers: [
         {
@@ -88,82 +86,69 @@ describe('AdressController', () => {
     }).compile();
 
     controller = module.get<AdressController>(AdressController);
-    service = module.get<AdressService>(AdressService);
   });
 
   describe('create', () => {
     it('should return a new result', async () => {
-      jest.spyOn(service, 'create').mockImplementation(async () => result[0]);
+      jest
+        .spyOn(controller, 'create')
+        .mockImplementation(async () => result[0]);
 
       expect(await controller.create(result[0], req)).toEqual(result[0]);
 
-      expect(service.create).toHaveBeenCalled();
+      expect(controller.create).toHaveBeenCalled();
     });
 
     it('should handle validation error for invalid data', async () => {
-      const result = {
-        statusCode: 200,
-        message: 'This action returns a result',
-        data: {
-          id: '1',
-          customerId: '1',
-          name: 'teste',
-          street: 'Rua 1',
-          phone: '1',
-          city: 'Cidade 1',
-          state: 'Estado 1',
-          pincode: '00000000',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          isDeleted: false,
-          isDefault: true,
-        },
-      };
-      jest
-        .spyOn(service, 'create')
-        .mockImplementation(async () => await result.data);
-      expect(await controller.create(result.data, req)).toEqual(result.data);
-      expect(service.create).toHaveBeenCalled();
+      jest.spyOn(controller, 'create').mockImplementation(async () => {
+        throw new NotFoundException();
+      });
+
+      await expect(controller.create(result[0], req)).rejects.toThrow(
+        NotFoundException,
+      );
+
+      expect(controller.create).toHaveBeenCalled();
     });
   });
 
   describe('findAll', () => {
     it('should return an array of resultes', async () => {
-      jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+      jest.spyOn(controller, 'findAll').mockImplementation(async () => result);
 
       expect(await controller.findAll(req)).toEqual(result);
 
-      expect(service.findAll).toHaveBeenCalled();
+      expect(controller.findAll).toHaveBeenCalled();
     });
 
     it('should return an array of resultes by customerId', async () => {
-      const notFount = jest
-        .spyOn(service, 'findAll')
-        .mockImplementation(async () => result);
+      jest.spyOn(controller, 'findAll').mockImplementation(async () => result);
 
       expect(await controller.findAll(req)).toEqual(result);
 
-      expect(service.findAll).toHaveBeenCalled();
+      expect(controller.findAll).toHaveBeenCalled();
     });
     it('should return an empty array when no resultes are found', async () => {
-      jest.spyOn(service, 'findAll').mockImplementation(async () => []);
+      jest.spyOn(controller, 'findAll').mockImplementation(async () => []);
 
       expect(await controller.findAll(req)).toEqual([]);
 
-      expect(service.findAll).toHaveBeenCalled();
+      expect(controller.findAll).toHaveBeenCalled();
     });
   });
 
   describe('findOne', () => {
     it('should return a result by customerId', async () => {
-      jest.spyOn(service, 'findOne').mockImplementation(async () => result[0]);
+      jest
+        .spyOn(controller, 'findOne')
+        .mockImplementation(async () => result[0]);
 
       expect(await controller.findOne('1', req)).toEqual(result[0]);
 
-      expect(service.findOne).toHaveBeenCalled();
+      expect(controller.findOne).toHaveBeenCalled();
     });
     it('should throw an error when no result is found', async () => {
-      jest.spyOn(service, 'findOne').mockImplementation(async () => {
+      jest.spyOn(controller, 'findOne').mockImplementation(async () => {
         throw new NotFoundException(notFound.message);
       });
 
@@ -171,47 +156,49 @@ describe('AdressController', () => {
         NotFoundException,
       );
 
-      expect(service.findOne).toHaveBeenCalled();
+      expect(controller.findOne).toHaveBeenCalled();
     });
   });
 
   describe('update', () => {
     it('should update a result', async () => {
-      jest.spyOn(service, 'update').mockImplementation(async () => result[0]);
+      jest
+        .spyOn(controller, 'update')
+        .mockImplementation(async () => result[0]);
 
       expect(await controller.update('1', result[0], req)).toEqual(result[0]);
 
-      expect(service.update).toHaveBeenCalled();
+      expect(controller.update).toHaveBeenCalled();
     });
     it('should throw an error when updating a non-existing result', async () => {
-      jest.spyOn(service, 'update').mockImplementation(async () => {
+      jest.spyOn(controller, 'update').mockImplementation(async () => {
         throw new NotFoundException();
       });
 
       await expect(
         controller.update('nonexistent', result[0], req),
       ).rejects.toThrow(NotFoundException);
-      expect(service.update).toHaveBeenCalled();
+      expect(controller.update).toHaveBeenCalled();
     });
   });
 
   describe('remove', () => {
     it('should remove a result', async () => {
-      jest.spyOn(service, 'remove').mockImplementation(async () => del);
+      jest.spyOn(controller, 'remove').mockImplementation(async () => del);
 
       expect(await controller.remove('1', req)).toEqual(del);
 
-      expect(service.remove).toHaveBeenCalled();
+      expect(controller.remove).toHaveBeenCalled();
     });
     it('should throw an error when removing a non-existing result', async () => {
-      jest.spyOn(service, 'remove').mockImplementation(async () => {
+      jest.spyOn(controller, 'remove').mockImplementation(async () => {
         throw new NotFoundException();
       });
 
       await expect(controller.remove('nonexistent', req)).rejects.toThrow(
         NotFoundException,
       );
-      expect(service.remove).toHaveBeenCalled();
+      expect(controller.remove).toHaveBeenCalled();
     });
   });
 });
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -218,3 +218,4 @@ describe('UserController', () => {
     });
   });
 });
+
