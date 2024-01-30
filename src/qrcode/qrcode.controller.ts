88966ee61f5769f44fb92544fb3594ca859664e2
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2024-01-30 11:25:00 +0530

    qr code bug fixes

--- src/qrcode/qrcode.controller.ts
+++ src/qrcode/qrcode.controller.ts
@@ -1,25 +1,13 @@
-// // src/qr-code.controller.ts
-// import { Controller, Get, Query } from '@nestjs/common';
-// import { QrCodeService } from './qrcode.service';
-// import { ApiProduces, ApiTags } from '@nestjs/swagger';
-
-// @ApiTags('QR Code')
-// @Controller('qr-code')
-// export class QrCodeController {
-//   constructor(private readonly qrCodeService: QrCodeService) {}
-
-//   @Get()
-//   @ApiProduces('image/png')
-//   async generateQrCode(@Query('data') data: string) {
-//     const qrCodeDataURL = await this.qrCodeService.generateQrCode(data);
-//     return `<img src="${qrCodeDataURL}" alt="QR Code" />`;
-//   }
-// }
-
 import { Controller, Get, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { QrCodeService } from './qrcode.service';
-import { ApiOkResponse, ApiOperation, ApiProduces, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiProduces,
+  ApiTags,
+  ApiBody,
+} from '@nestjs/swagger';
 
 @ApiTags('QR Code')
 @Controller('qr-code')
@@ -38,9 +26,15 @@ export class QrCodeController {
     },
   })
   async generateQrCode(@Query('data') data: string, @Res() res: Response) {
-    const qrCodeDataURL = await this.qrCodeService.generateQrCode(data);  
+    const qrCodeDataURL = await this.qrCodeService.generateQrCode(data);
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Content-Disposition', 'inline; filename=qr-code.png');
+    const buffer = Buffer.from(qrCodeDataURL.split(',')[1], 'base64');
 
-    // Send the image data as the response
-    return res.send(Buffer.from(qrCodeDataURL.split(',')[1], 'base64'));
+    // Convert binary image data to data URI
+    console.log(buffer);
+
+    // Send the data URI as the response
+    return res.send(buffer);
   }
 }
--- src/qrcode/qrcode.service.ts
+++ src/qrcode/qrcode.service.ts
@@ -1,12 +1,16 @@
 // src/qr-code.service.ts
 import { Injectable } from '@nestjs/common';
 import * as qrcode from 'qrcode';
+import { buffer } from 'stream/consumers';
 
 @Injectable()
 export class QrCodeService {
   async generateQrCode(data: string): Promise<string> {
     try {
       const qrCodeDataURL = await qrcode.toDataURL(data);
+
+      const buffer = Buffer.from(qrCodeDataURL.split(',')[1], 'base64');
+
       return qrCodeDataURL;
     } catch (error) {
       throw new Error('Failed to generate QR code.');
